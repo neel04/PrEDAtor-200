6522a971099cd959921bf89d4e69412ea9aceac2
neel04 <neelgupta04@outlook.com>
2022-02-19 16:56:35 +0000

    Added vanilla sumary

--- train_vqvae.py
+++ train_vqvae.py
@@ -28,6 +28,7 @@ def train(loader, val_loader, scheduler):
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     accelerator.print(summary(model, (args.batch_size, 3, args.size, args.size)))
+    accelerator.print(model) #vanilla pytorch summary
 
     model, optimizer, loader, val_loader = accelerator.prepare(model, optimizer, loader, val_loader)
     loader, val_loader = tqdm(loader), tqdm(val_loader)
