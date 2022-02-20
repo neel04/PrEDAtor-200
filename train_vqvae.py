577d3fbe3f601bd08c55b20becc9d496cfd4c021
neel04 <neelgupta04@outlook.com>
2022-02-20 00:38:58 +0000

    Downed prefetch

--- train_vqvae.py
+++ train_vqvae.py
@@ -168,10 +168,10 @@ if __name__ == '__main__':
     val_dataset = datasets.ImageFolder(args.validation_path, transform=transform)
 
     loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, #True
-                        num_workers=args.num_workers, pin_memory=True, prefetch_factor=args.batch_size // 2)
+                        num_workers=args.num_workers, pin_memory=True, prefetch_factor=32)
 
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
-                        num_workers=args.num_workers, pin_memory=True, prefetch_factor=args.batch_size // 2)
+                        num_workers=args.num_workers, pin_memory=True, prefetch_factor=32)
 
     model = VQVAE(in_channel=3, channel=128, n_res_block=args.res_blocks,
                   n_res_channel=args.res_channel,
